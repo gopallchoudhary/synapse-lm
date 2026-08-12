3cab1ee9efaba3009b408056633a2155e15fc7e4
gopallchoudhary <gopalchoudhary521@gmail.com>
2026-08-12 15:26:35 +0530

    feat: update Home component styling and add PostCSS configuration for Tailwind CSS

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ import { ThemeToggle } from "~/components/theme-toggle";
 export default function Home() {
 	return (
 		<div>
-			<h1 className="text-8xl text-red-900">Synapse LM</h1>
+			<h1 >Synapse LM</h1>
 			<div>
 				<ThemeToggle />
 			</div>
